786d852ffb4e89052597b218cd9b80d53d0bed7d
Renzogio-Garcia <36797283+Renzogio-Garcia@users.noreply.github.com>
2024-12-17 17:34:59 -0500

    Update FDAdata_test.py

--- tests/FDAdata_test.py
+++ tests/FDAdata_test.py
@@ -8,13 +8,13 @@ def test_fetch_drug_data():
     drug_data_zemplar = fetch_drug_data("drug", "zemplar")
 
     assert drug_data_pfizer[0]["recalling_firm"] == "Pfizer Inc."
-    assert drug_data_pfizer[0]["brand_name"][0] == "ZEMPLAR"
+    assert drug_data_pfizer[0]["openfda"]["brand_name"][0] == "ZEMPLAR"
 
 def test_fetch_device_data():
     pma_pfizer, recall_pfizer = fetch_device_data("company", "Pfizer")
     
     assert pma_pfizer[0]["applicant"] == "PFIZER, INC."
-    assert recall_pfizer[0]["recalling_firm"] == "Pfizer Inc."
+    assert recall_pfizer[0]["recalling_firm"] == "Pfizer Global"
 
 def test_process_recall_data():
     drug_data_zemplar = fetch_drug_data("drug", "zemplar")
